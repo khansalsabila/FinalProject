49543d75e705372a4db6961e0f69f0579d2d03fb
khansalsabila <lathifahks1702@gmail.com>
2023-01-12 09:52:18 +0700

    menambahkan mycontroller

--- src/main/java/FinalProject/finalproject/MyController.java
+++ src/main/java/FinalProject/finalproject/MyController.java
@@ -4,10 +4,70 @@
  */
 package FinalProject.finalproject;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author USER DJOGJA
  */
+
+@RestController
+@CrossOrigin
+
 public class MyController {
-    
+        KependudukanJpaController control = new KependudukanJpaController();
+        
+        @PostMapping("/POST")
+	public String sendData(HttpEntity<String> kiriman) throws Exception{
+		Kependudukan datas = new Kependudukan();
+		String d = kiriman.getBody();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		datas = mapper.readValue(d, Kependudukan.class);
+	        control.create(datas);
+		return d;
+	}
+	
+	@PutMapping("/PUT")
+	public String editData(HttpEntity<String> kiriman) throws Exception{
+		Kependudukan datas = new Kependudukan();
+		String d = kiriman.getBody();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		datas = mapper.readValue(d, Kependudukan.class);
+	        control.edit(datas);
+		return d;
+	}
+	
+	@DeleteMapping("/DELETE")
+	public String deleteData(HttpEntity<String> kiriman) throws Exception{
+		Kependudukan datas = new Kependudukan();
+		String d = kiriman.getBody();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		datas = mapper.readValue(d, Kependudukan.class);
+	        control.destroy(datas.getId());
+		return "id: "+datas.getId()+" deleted";
+	}
+	
+	@GetMapping("/GET")
+	public List<Kependudukan> getTabel(){
+		List<Kependudukan> list = new ArrayList<>();
+		try {
+			list = control.findKependudukanEntities();
+		}
+		catch (Exception e){}
+		return list;
+	}
+
 }
